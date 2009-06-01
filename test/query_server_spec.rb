f468beb176d8c3d990614ec1b1261228f36dd343
Chris Anderson <jchris@grabb.it>
2009-06-01 14:19:33 -0700

    specs passing

--- test/query_server_spec.rb
+++ test/query_server_spec.rb
@@ -24,7 +24,7 @@ require 'json'
 
 class OSProcessRunner
   def self.run
-    trace = true
+    trace = false
     puts "launching #{run_command}" if trace
     if block_given?
       Open3.popen3(run_command) do |jsin, jsout, jserr|
