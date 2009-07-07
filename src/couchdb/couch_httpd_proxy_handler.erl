838748f3b21ced0d12dd4e00a0215520309bc1b0
Benoit Chesneau <bchesneau@gmail.com>
2009-07-07 19:37:41 +0200

    stream proxy response

--- src/couchdb/couch_httpd_proxy_handler.erl
+++ src/couchdb/couch_httpd_proxy_handler.erl
@@ -15,6 +15,7 @@
 -export([handle_proxy_req/2]).
     
 -include("couch_db.hrl").
+-include("../ibrowse/ibrowse.hrl").
 
 handle_proxy_req(#httpd{mochi_req=MochiReq}=Req, DestPath) ->
     DestPath1 = parse_dest_path(DestPath, Req),
@@ -31,42 +32,182 @@ handle_proxy_req(#httpd{mochi_req=MochiReq}=Req, DestPath) ->
                     mochiweb_headers:to_list(MochiReq:get(headers))),
         Method = mochiweb_to_ibrowse_method(MochiReq:get(method)),
         ReqBody = get_body(couch_httpd:body(Req)),
-        do_proxy_request(Req, {RawPath, Headers, Method, ReqBody}, _ActionKey, DestPath1);
+        case do_proxy_request(RawPath, Headers, Method, ReqBody) of
+            {redirect, RedirectUrl, _} ->
+                case mochiweb_util:partition(RedirectUrl, DestPath1) of
+                     {"", _, RelPath} ->
+                          RedirectPath = lists:append(["/", _ActionKey, RelPath]),
+                          ?LOG_DEBUG("redirect to ~s", [RedirectPath]),
+                          couch_httpd:send_redirect(Req, RedirectPath);
+                     {_, "", ""} ->
+                         couch_httpd:send_json(Req, 502, {[{error, <<"Bad Gateway">>}, {reason, << "Bad redirection" >>}]})
+                end;
+            {ok, RcvFun, {Status, RespHeaders}} ->
+                %%RespHeaders1 = fix_location(RespHeaders, {_ActionKey, DestPath1}),
+                ?LOG_DEBUG("proxy response status ~p", [Status]),
+                {ok, Resp} = couch_httpd:start_chunked_response(Req, Status, RespHeaders),
+                proxy_respond(RcvFun, Resp);
+            {error, ResponseCode} ->
+                couch_httpd:send_error(Req, ResponseCode);
+            _ ->
+                couch_httpd:send_json(Req, 502, {[{error, <<"Bad Gateway">>}]})
+        end;
+                
     {_ActionKey, "", _RelativePath} ->
         RedirectPath = couch_httpd:path(Req) ++ "/",
         couch_httpd:send_redirect(Req, RedirectPath)
 end;
 handle_proxy_req(Req, _) ->
     couch_httpd:send_method_not_allowed(Req, "").
+
+proxy_respond(Source, State) when is_function(Source) ->
+    proxy_respond({Source}, State);
+proxy_respond({Source}, State) when is_function(Source) ->
+    try 
+        proxy_respond1(Source, Source(), State)
+    catch 
+        _:_ -> couch_httpd:send_chunk(State, "")
+    end;
+proxy_respond(Body, State) ->
+    couch_doc:bin_foldl(Body,
+        fun(BinSegment, _) -> couch_httpd:send_chunk(State, BinSegment) end,[]),
+    couch_httpd:send_chunk(State, "").
+
+proxy_respond1(Source, Resp, State) ->
+    case Resp of
+    {ok, ""} ->
+        couch_httpd:send_chunk(State, "");
+	{ok, Data} ->
+	    couch_httpd:send_chunk(State, Data),
+	    proxy_respond({Source}, State);
+	_ ->
+	    couch_httpd:send_chunk(State, "")
+    end.
+
+proxy_loop(ReqId, Conn) ->
+    couch_util:should_flush(),
+    receive
+        {From, {set_req_id, NewId}} ->
+            %% we learn the ReqId to listen for
+            From ! {self(), {ok, NewId}},
+            proxy_loop(NewId, Conn);
+        {ibrowse_async_headers, ReqId, Status, Headers} ->
+            %% we got header, give the controlling process a chance to react
+            receive
+                {From, gimme_status} ->
+                    %% send status/headers to controller
+                    From ! {self(), {status, Status, Headers}},
+                    receive
+                        {From, continue} ->
+                            %% normal case
+                            proxy_loop(ReqId, Conn);
+                        {From, fail} ->
+                            %% error, failure code
+                            ?LOG_ERROR(
+                                "streaming proxy body failed with status ~p",
+                                [Status]),
+                            catch ibrowse:stop_worker_process(Conn),
+                            exit(proxy_request_failed);
+                        {From, stop_ok} ->
+                            %% stop looping, controller will start a new loop
+                            catch ibrowse:stop_worker_process(Conn),
+                            stop_ok
+                    end
+            end,
+            proxy_loop(ReqId, Conn);
+        {ibrowse_async_response, ReqId, {chunk_start,_}} ->
+            proxy_loop(ReqId, Conn);
+        {ibrowse_async_response, ReqId, chunk_end} ->
+            proxy_loop(ReqId, Conn);
+        {ibrowse_async_response, ReqId, {error, Err}} ->
+            ?LOG_ERROR("streaming attachment failed with ~p", [Err]),
+            catch ibrowse:stop_worker_process(Conn),
+            exit(proxy_request_failed);
+        {ibrowse_async_response, ReqId, Data} ->
+            receive {From, gimme_data} ->  From ! {self(), Data} end,
+            proxy_loop(ReqId, Conn);
+        {ibrowse_async_response_end, ReqId} ->
+            receive {From, gimme_data} ->  From ! {self(), ""} end,
+            catch ibrowse:stop_worker_process(Conn),
+            exit(normal)
+    end.  
     
-do_proxy_request(Req, {P, H, M, B}, SrcPath, DestPath) ->
-    case ibrowse:send_req(P, H, M, B) of
-         {ok, Status, RespHeaders, RespBody} ->
-             case is_redirect(list_to_integer(Status)) of
-                 true ->
-                     {LocationHeader, _} = proplists:split(RespHeaders, ["Location"]),
-                     [LocationHeader1|_] = LocationHeader,
-                     Path = proplists:get_value("Location", LocationHeader1),
-                     case mochiweb_util:partition(Path, DestPath) of
-                         {"", _, RelPath} ->
-                              RedirectPath = lists:append(["/", SrcPath, RelPath]),
-                              ?LOG_DEBUG("redirect to ~s", [RedirectPath]),
-                              couch_httpd:send_redirect(Req, RedirectPath);
-                         {_, "", ""} ->
-                             couch_httpd:send_json(Req, 502, {[{error, <<"Bad Gateway">>}, {reason, << "Bad redirection" >>}]})
-                    end;    
-                 false ->
-                    RespHeaders1 = fix_location(RespHeaders, {SrcPath, DestPath}),
-                    ?LOG_DEBUG("httpd ~p proxy response headers:~n ~p", [list_to_integer(Status), 
-                                                                        RespHeaders]),
-                    Body = list_to_binary(get_body(RespBody)),
-                    {ok, Resp} = couch_httpd:start_chunked_response(Req, list_to_integer(Status), RespHeaders1),
-                    couch_doc:bin_foldl(Body,
-                            fun(BinSegment, _) -> couch_httpd:send_chunk(Resp, BinSegment) end,[]),
-                    couch_httpd:send_chunk(Resp, "")
-            end;
-        {error, Reason} ->
-            couch_httpd:send_json(Req, 502, {[{error, <<"Bad Gateway">>}, {reason, << Reason >>}]})
+do_proxy_request(Url, Headers, Method, Body) ->
+    do_proxy_request(Url, Headers, Method, Body, 3, 1000).
+
+do_proxy_request(Url, _Headers, _Method, _Body, 0, _Pause) ->
+    {proxy_request_failed, ?l2b(["failed to fetch response ", Url])};
+    
+do_proxy_request(Url, Headers, Method, Body, Retries, Pause) ->
+    #url{host=Host, port=Port} = ibrowse_lib:parse_url(Url),
+    {ok, Conn} = ibrowse:spawn_link_worker_process(Host, Port),
+    Pid = spawn_link(fun() -> proxy_loop(nil, Conn) end),
+    Opts = [{stream_to, Pid}, {response_format, binary}],
+    ReqId = 
+    case ibrowse:send_req_direct(Conn, Url, Headers, Method, Body, Opts, infinity) of
+    {ibrowse_req_id, X} ->
+             X;
+    {error, Reason} ->
+        ?LOG_INFO("error while fetching body due to {error, ~p } on ~s", [Reason, Url]),
+        catch ibrowse:stop_worker_process(Conn),
+        timer:sleep(Pause),
+        do_proxy_request(Url, Headers, Method, Body,
+        Retries-1, 2*Pause)
+    end,
+    Pid ! {self(), {set_req_id, ReqId}},
+    receive
+    {Pid, {ok, ReqId}} ->
+        ok;
+    {'EXIT', Pid, _Reason} ->
+        catch ibrowse:stop_worker_process(Conn),
+        timer:sleep(Pause),
+        do_proxy_request(Url, Headers, Method, Body,
+            Retries-1, 2*Pause)
+    end,
+    Pid ! {self(), gimme_status},
+    receive
+        {'EXIT', Pid, proxy_request_failed} ->
+             catch ibrowse:stop_worker_process(Conn),
+             do_proxy_request(Url, Headers, Method, Body,
+                     Retries-1, Pause);
+        {Pid, {status, StreamStatus, StreamHeaders}} ->
+            ?LOG_DEBUG("streaming proxy response Status ~p Headers ~p",
+                [StreamStatus, StreamHeaders]),
+            ResponseCode = list_to_integer(StreamStatus),
+            if
+            ResponseCode >= 200, ResponseCode < 300 ->
+                Pid ! {self(), continue},
+                {ok, fun() ->
+                    Pid ! {self(), gimme_data},
+                    receive
+                        {Pid, Data} ->
+                            {ok, Data};
+                        {Pid, ""} ->
+                            eof;
+                        {'EXIT', Pid, proxy_request_failed} ->
+                            throw(proxy_response_failed)
+                    end
+                end, {ResponseCode, StreamHeaders}};
+             ResponseCode >= 300, ResponseCode < 400 ->
+                 Pid ! {self(), stop_ok},
+                 RedirectUrl = mochiweb_headers:get_value("Location",
+                     mochiweb_headers:make(StreamHeaders)),
+                 {redirect, RedirectUrl, StreamHeaders};
+             ResponseCode >= 400, ResponseCode < 500 ->
+                 ?LOG_ERROR("streaming proxy response failed with code ~p: ~s",
+                     [ResponseCode, Url]),
+                 Pid ! {self(), fail},
+                 {error, ResponseCode};
+             ResponseCode == 500 ->
+                 % an error... log and retry
+                 ?LOG_INFO("retrying proxy request in ~p " ++
+                     "seconds due to 500 response: ~s", [Pause/1000, Url]),
+                 Pid ! {self(), fail},
+                 catch ibrowse:stop_worker_process(Conn),
+                 timer:sleep(Pause),
+                 do_proxy_request(Url, Headers, Method, Body,
+                     Retries-1, 2*Pause)
+        end
     end.
           
 is_redirect(Status) ->
@@ -96,7 +237,6 @@ fix_location([{"Location", ProxyDataPath}|Rest],
     [{"Location", DestPath++ProxyPath}|Rest];
 fix_location([H|T], C) ->
     [H|fix_location(T, C)].
-  
   
 parse_dest_path(DestPath, Req) when is_binary(DestPath) ->
     parse_dest_path(?b2l(DestPath), Req);
